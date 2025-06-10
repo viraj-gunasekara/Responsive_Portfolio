57ee020304225e73949027bf954529d16e289745
viraj-gunasekara <vsgunasekara.dev@gmail.com>
2025-06-11 02:21:00 +0530

    send emails via contact form - EmailJs

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,7 +36,33 @@ const shadowHeader = () =>{
 window.addEventListener('scroll', shadowHeader)
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message')
 
+const sendEmail = (e) =>{
+    e.preventDefault()
+
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_***','template_***','#contact-form','***')
+    .then(() =>{
+        // Show sent message
+        contactMessage.textContent = 'Message sent successfully ✅'
+
+        // Remove message after five seconds
+        setTimeout(() =>{
+          contactMessage.textContent = ''
+        }, 6000)
+
+        // Clear input fields
+        contactForm.reset()
+
+    }, () =>{
+          // Show error message
+          contactMessage.textContent = 'Message not sent (service error) ❌'
+    })
+}
+
+contactForm.addEventListener('submit', sendEmail)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
